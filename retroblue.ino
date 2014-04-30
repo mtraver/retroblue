a2ec953c7d4cd90428209b7bda4fb205b5028f80
Michael Traver <mtraver@college.harvard.edu>
2014-04-30 16:53:27 -0400

    Add basic Arduino sketch

---
+++ retroblue.ino
@@ -0,0 +1,223 @@
+/* Arduino code used to turn a vintage rotary phone
+ * into a 21st century Bluetooth accessory.
+ * 
+ * Project (most specifically, the code that detects
+ * dialed numbers) based on tutorial and code at
+ * https://www.sparkfun.com/products/retired/8929.
+ */
+
+#include <SoftwareSerial.h>
+
+/* pins used to detect the signals from the phone */
+const int pulsePin = 2;
+const int dialingPin = 3;
+const int hookPin = 4;
+
+/* pins used to communicate with the Bluetooth module */
+const int eventPin = 7;
+const int resetPin = 12;
+
+/* state of pulse switch
+ * 1 during dialing pulse
+ * 0 otherwise
+ */
+int pulse;
+const int PULSE = 1;
+const int NO_PULSE = 0;
+
+/* state of dial
+ * 0 if a number is being dialed (i.e. any time dial is not in rest position)
+ * 1 if dial is in rest position
+ */
+int dialing;
+const int IS_DIALING = 0;
+const int NOT_DIALING = 1;
+
+/* state of hook
+ * 0 if phone off hook
+ * 1 if phone on hook
+ */
+int hook;
+const int OFF_HOOK = 0;
+const int ON_HOOK = 1;
+
+/* command and response line endings  */
+String COMMAND_TERMINATOR = "\r";
+String RESPONSE_TERMINATOR = "\r\n";
+
+/* the amount of time we give the Bluetooth module to send a response, in ms */
+const int RESPONSE_WAIT_TIME = 3000;
+
+/* Arduino Uno only has one hardware serial interface, so set up a software
+ * serial interface for communicating with the Bluetooth module.
+ *
+ * RX = digital pin 10, TX = digital pin 11
+ */
+SoftwareSerial bluetooth(10, 11);
+
+void setup() {
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+
+  /* tell pin to use internal pull-up resistor */
+  pinMode(pulsePin, INPUT);
+  digitalWrite(pulsePin, HIGH);
+  
+  /* tell pin to use internal pull-up resistor */
+  pinMode(dialingPin, INPUT);
+  digitalWrite(dialingPin, HIGH);
+  
+  /* tell pin to use internal pull-up resistor */
+  pinMode(hookPin, INPUT);
+  digitalWrite(hookPin, HIGH);
+  
+  /* tell pin to use internal pull-up resistor */
+  pinMode(eventPin, INPUT);
+  digitalWrite(eventPin, HIGH);
+  
+  pinMode(resetPin, OUTPUT);
+
+  bluetooth.listen();
+}
+
+/* reads signals from the phone and updates dialing state variables */
+void readState() {
+  pulse = digitalRead(pulsePin);
+  dialing = digitalRead(dialingPin);
+  hook = digitalRead(hookPin);
+}
+
+/* properly terminates the given command, sends it to the Bluetooth module, and waits for a response */
+void issueCommand(String command, boolean checkForTerminator) {
+  Serial.println("Issuing command '" + command + "'");
+  
+  bluetooth.print(command + COMMAND_TERMINATOR);
+  
+  unsigned long startTime = millis();
+  String buffer;
+  while (startTime + RESPONSE_WAIT_TIME > millis()) {
+    if (bluetooth.available() > 0) buffer.concat(char(bluetooth.read()));
+    
+    // TODO chop off terminator before returning response
+    if (checkForTerminator && buffer.endsWith(RESPONSE_TERMINATOR)) {
+      Serial.println("Got EOL. Printing buffer:");
+      Serial.println(buffer);
+      return buffer;
+    }
+  }
+  
+  Serial.println("End of wait. Printing buffer:");
+  Serial.println(buffer);
+  return buffer;
+}
+
+String dialNumber(String num) {
+  // TODO remove when done debugging
+  num = "REDACTED";
+
+  return issueCommand("A," + num, true);
+}
+
+String acceptCall() {
+  return issueCommand("C", true);
+}
+
+String endCall() {
+  return issueCommand("E", true);
+}
+
+String getStatus() {
+  return issueCommand("Q", true);
+}
+
+String getSettings() {
+  return issueCommand("D", false);
+}
+
+String getModuleHelp() {
+  return issueCommand("H", false);
+}
+
+String getFirmwareVersion() {
+  return issueCommand("V", false);
+}
+
+void loop() {
+  readState();
+
+  // TODO When an event happens, the pin is held low for 100ms. Should
+  // we be checking how long it's low just to make sure that we don't
+  // check the status if some noise happens to come through?
+  int event = digitalRead(eventPin);
+  if (event == 0) {
+    Serial.println("Event detected!");
+
+    String status = getStatus();
+    Serial.println(status);
+  }
+  
+  if (hook == OFF_HOOK) {
+    Serial.println("OFF hook");
+    
+    String numberString = "";
+    boolean doDialNumber = true;
+    
+    for (int numCount = 0; numCount < 10; numCount++) {
+      int dialedNumber = 0;
+      
+      /* wait for the user to start dialing a number */
+      readState();
+      while (dialing == NOT_DIALING && hook == OFF_HOOK) {
+        readState();
+      }
+      delay(15);
+      
+      /* while the dial is returning... */
+      readState();
+      while (dialing == IS_DIALING && hook == OFF_HOOK) {
+        /* wait for pulse interval to finish */
+        readState();
+        while (pulse == PULSE) {
+          readState();
+          if (dialing == NOT_DIALING || hook == ON_HOOK) break;
+        }
+        delay(70);
+        
+        /* wait for between-pulse interval to finish */
+        readState();
+        while (pulse == NO_PULSE) {
+          readState();
+          if (dialing == NOT_DIALING || hook == ON_HOOK) break;
+        }
+        delay(33);
+        
+        dialedNumber++;
+      }
+      
+      readState();
+      if (hook == ON_HOOK) {
+        /* phone was hung up, so break */
+        Serial.println("hung up!");
+        doDialNumber = false;
+        break;
+      }
+      
+      /* one extra pulse is registered when dialing (i.e. 5 is sensed when 4 is dialed), so decrement */
+      dialedNumber--;
+
+      /* the phone dials a 0 as if it were a 10, so fix that */
+      dialedNumber = dialedNumber % 10;
+      
+      numberString = numberString + dialedNumber;
+      
+      Serial.print(dialedNumber);
+      if (numCount == 2 || numCount == 5) Serial.print("-");
+    }
+    
+    Serial.print("\n");
+    if (doDialNumber) dialNumber(numberString);
+  }
+  // else {
+  //   Serial.println("on hook");
+  // }
+}
